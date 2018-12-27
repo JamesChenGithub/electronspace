d8c9d866cac68d0c5b31eab264083fa3b27fc78c
alexichen <alexichen@tencent.com>
2018-12-27 15:26:51 +0800

    添加测试工程

---
+++ vkdemo/index.js
@@ -0,0 +1,28 @@
+const {app, BrowserWindow} = require('electron')
+
+let mainWindow;
+
+
+
+app.on('ready', function(){
+    mainWindow  = new BrowserWindow({width:800, height:600})
+    mainWindow.loadFile('index.html')
+
+    mainWindow.on('close', function(){
+        mainWindow = null
+    })
+})
+
+app.on('windows-all-closed', function(){
+    if (process.platform !== 'darwin') {
+        application.quit()
+    }
+})
+
+app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+      createWindow()
+    }
+  })
